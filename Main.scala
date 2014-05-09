849cc779c3278b1b1dbf0abef49f9231ff72c42e
Kevin Kyyro <kkyyro@linkedin.com>
2014-05-08 19:32:02 -0700

    initial commit

---
+++ Main.scala
@@ -0,0 +1,5 @@
+package scala.processing.main
+
+object Processing {
+  def main(args: Array[String]): Unit = println("Hello, world")
+}
---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala-processing"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
